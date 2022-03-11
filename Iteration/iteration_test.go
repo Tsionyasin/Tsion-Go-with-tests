ecc78f11282e8d278e6302d5089461b75245ec8b
TSION <tsitiyasin6@gmail.com>
2022-03-11 22:58:51 +0300

    Repeat with Benchmark

--- Iteration/iteration_test.go
+++ Iteration/iteration_test.go
@@ -11,6 +11,17 @@ func TestRepeat(t *testing.T) {
 	}
 }
 
-func Repeat(name string) string {
-	return "aaaaa"
+const repeatCount = 5
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
 }
