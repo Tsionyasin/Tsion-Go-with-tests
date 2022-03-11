b88a7ea39a480402378daafdeea681a40f4ee6ec
TSION <tsitiyasin6@gmail.com>
2022-03-11 15:57:38 +0300

    more requirements adding language parameter

--- Hello_world/hello.go
+++ Hello_world/hello.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHellowPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+
+	if language == "French" {
+		return frenchHellowPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
