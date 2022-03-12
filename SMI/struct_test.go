9a166a220169938c2f2eb8d72be10823296910ef
TSION <tsitiyasin6@gmail.com>
2022-03-12 12:09:35 +0300

    test on interface

--- SMI/struct_test.go
+++ SMI/struct_test.go
@@ -3,13 +3,24 @@ package smi
 import "testing"
 import "math"
 
+type Shape interface {
+	Area() float64
+}
 type Rectangle struct {
 	Width  float64
+	Height float64
+}
+type Triangle struct {
+	Base   float64
 	Height float64
 }
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 type Circle struct {
@@ -35,28 +46,28 @@ func TestParameter(t *testing.T) {
 }
 func TestArea(t *testing.T) {
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	}
 
 }
-
 func Parameter(rectangle Rectangle) float64 {
 
 	return 2 * (rectangle.Width + rectangle.Height)
