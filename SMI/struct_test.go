716ec42de4ad7a583ec20fd3d89466e3ed519d86
TSION <tsitiyasin6@gmail.com>
2022-03-12 11:45:19 +0300

    testing on methods

--- SMI/struct_test.go
+++ SMI/struct_test.go
@@ -1,10 +1,23 @@
 package smi
 
 import "testing"
+import "math"
 
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func TestParameter(t *testing.T) {
@@ -22,15 +35,25 @@ func TestParameter(t *testing.T) {
 }
 func TestArea(t *testing.T) {
 
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.00
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 
-		t.Errorf("got %.2f, want %.2f", got, want)
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
 
-	}
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 
 }
 
