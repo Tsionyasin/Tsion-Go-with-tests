89f4f475e8327fd2331053c589f65b9fef36dda2
TSION <tsitiyasin6@gmail.com>
2022-03-14 00:32:39 +0300

    update

--- Pointers/wallet_test.go
+++ Pointers/wallet_test.go
@@ -1,43 +1,7 @@
-package pointer
+package main
 
-import (
-	"errors"
-	"fmt"
-	"testing"
-)
+import "testing"
 
-var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
-
-type Bitcoin int
-
-type Stringer interface {
-	String() string
-}
-
-type Wallet struct {
-	balance Bitcoin
-}
-
-func (w *Wallet) Deposit(amount Bitcoin) {
-	w.balance += amount
-}
-
-func (w *Wallet) Withdraw(amount Bitcoin) error {
-
-	if amount > w.balance {
-		return ErrInsufficientFunds
-	}
-
-	w.balance -= amount
-	return nil
-}
-
-func (w *Wallet) Balance() Bitcoin {
-	return w.balance
-}
-func (b Bitcoin) String() string {
-	return fmt.Sprintf("%d BTC", b)
-}
 func TestWallet(t *testing.T) {
 
 	t.Run("Deposit", func(t *testing.T) {
@@ -62,31 +26,4 @@ func TestWallet(t *testing.T) {
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, Bitcoin(20))
 	})
-}
-
-func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
-	t.Helper()
-	got := wallet.Balance()
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
-}
-
-func assertNoError(t testing.TB, got error) {
-	t.Helper()
-	if got != nil {
-		t.Fatal("got an error but didn't want one")
-	}
-}
-
-func assertError(t testing.TB, got error, want error) {
-	t.Helper()
-	if got == nil {
-		t.Fatal("didn't get an error but wanted one")
-	}
-
-	if got != want {
-		t.Errorf("got %s, want %s", got, want)
-	}
 }
